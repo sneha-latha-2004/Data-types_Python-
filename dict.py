38b0315abeba053a89a3f0daf41950ab26c4b0ff
sneha-latha-2004 <sneha22ec052@drttit.edu.in>
2026-03-25 12:00:03 +0530

    dict

---
+++ dict.py
@@ -0,0 +1,15 @@
+# Creating dictionary
+student = {
+    "name": "Sneha",
+    "age": 18,
+    "course": "ECE"
+}
+
+# Accessing values
+print("Name:", student["name"])
+
+# Adding new key
+student["marks"] = 90
+
+# Printing dictionary
+print("Dictionary:", student)
