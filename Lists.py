226a371c7f5b95a863c3bd82fe2c85a0738e607b
sneha-latha-2004 <sneha22ec052@drttit.edu.in>
2026-03-25 11:53:13 +0530

    lists

---
+++ Lists.py
@@ -0,0 +1,11 @@
+#Creating list
+numbers = [1, 2, 3, 4, 5]
+
+# Adding element
+numbers.append(6)
+
+# Printing list
+print("List elements:", numbers)
+
+# Accessing element
+print("First element:", numbers[0])
